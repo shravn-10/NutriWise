30606055248d24354475a5a617ea18f88504199c
Karthik Rao <karapril15@gmail.com>
2024-07-05 09:22:32 +0530

    Avoid duplicate likes in likedRecipies

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ let rightArrowClickCount = 0;
 
 async function fetchRecipes() {
     try {
-        const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&app_id=22a10e41&app_key=%203c8283966b138fb4e09ac5ec5202e7f2%09&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Caribbean&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&random=true&field=label&field=image&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=calories&field=cuisineType&field=mealType&field=dishType');
+        const response = await fetch('https://api.edamam.com/api/recipes/v2?type=public&app_id=22a10e41&app_key=%203c8283966b138fb4e09ac5ec5202e7f2&health=low-sugar&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Caribbean&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&field=label&field=image&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=calories&field=cuisineType&field=mealType&field=dishType&random=true');
         const data = await response.json();
         
         recipes = data.hits.map(hit => hit.recipe);
@@ -64,38 +64,36 @@ rightArrow.addEventListener('click', async () => {
 
 likeButton.addEventListener('click', () => {
     const recipe = recipes[currentRecipeIndex];
-    likedRecipes.push({ label: recipe.label, uri: recipe.url });
-    alert(`Liked! Recipes saved: ${likedRecipes.map(item => item.label).join(', ')}`);
+    const recipeTuple = { label: recipe.label, uri: recipe.url };
+    const isDuplicate = likedRecipes.some(likedRecipe => likedRecipe.label === recipeTuple.label && likedRecipe.uri === recipeTuple.uri);
+    
+    if (!isDuplicate) {
+        likedRecipes.push(recipeTuple);
+        alert(`Liked! Recipes saved: ${likedRecipes.map(item => item.label).join(', ')}`);
+    } else {
+        alert('This recipe is already liked!');
+    }
 });
 
 nahButton.addEventListener('click', async () => {
-    const recipe = recipes[currentRecipeIndex];
-    const cuisines = recipe.cuisineType || [];
-
     recipes.splice(currentRecipeIndex, 1);
 
     if (recipes.length === 0) {
         await fetchRecipes();
     } else {
-        const filteredRecipes = recipes.filter(r => cuisines.some(c => r.cuisineType && r.cuisineType.includes(c)));
-        
-        if (filteredRecipes.length > 0) {
-            recipes = filteredRecipes;
-            currentRecipeIndex = 0;
-        } else {
-            await fetchRecipes();
+        if (currentRecipeIndex >= recipes.length) {
+            currentRecipeIndex = recipes.length - 1;
         }
+        displayRecipe(currentRecipeIndex);
     }
-
-    displayRecipe(currentRecipeIndex);
 });
 
 function NavigateToRecipeFinder() {
-    window.location.href = "pages/recipeFinder.html";
+    window.location.href = `pages/recipeFinder.html`;
 }
 
 function NavigateToFirstPage() {
-    window.location.href = "pages/first_page.html";
+    window.location.href = `pages/first_page.html`;
 }
 
 fetchRecipes();
